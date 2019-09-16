be3a488a16cfff967ba1bdeb01c301db79698783
Gleb Besarab <gleb.besarab@tideworks.com>
2019-09-16 15:21:50 +0300

    Add CLIArgument parser

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 import shutil
 import requests
-import sys, os, errno
+import sys, os, errno, argparse
 
 
 #read list of card name from file
@@ -11,39 +11,103 @@ import sys, os, errno
 #print cards that have problems with pulling
 
 
-file_name = sys.argv[1]
+class CLIArguments() :
+    PREFIX="--"
+    CARDS_LIST_FILE_PATH="card_list_file_path"
 
-#file_name = input("Enter path to file with deck: ")
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
 
-with open(file_name) as file:
-    lines = list(file)
-    deck_name = file_name.split(".")[0]
+    def defineArguments(self):
+        self.addArgument(self.CARDS_LIST_FILE_PATH, "Path to file with cards list")
+        return self
 
-    for line in lines:
-        strings = line.split(" ")
-        del strings[0]
-        card_name = "".join(strings)
+    def parse(self):
+        self.args = self.parser.parse_args()
+        return self
 
-        URL = "https://api.scryfall.com/cards/search"
-          
-        PARAMS = {'q':card_name} 
-              
-        print("Pulling {} card.....".format(card_name))
-        r = requests.get(url = URL, params = PARAMS) 
+    def addArgument(self, argumentName, argumentDescription):
+        self.parser.add_argument(self.PREFIX+argumentName, help=argumentDescription)
 
-        data = r.json()
+    def getArgument(self, argumentName):
+        return getattr(self.args, argumentName)
 
-        png_img_uri=data['data'][0]['image_uris']['png']
 
-        response = requests.get(png_img_uri, stream=True)
-        
-        if '//' in card_name:
-            card_name = card_name.split('//')[0]
-        png_name = './' + deck_name + '/' + card_name + '.png'
+class Main():
+    def main(self):
+        cliArguments = CLIArguments().defineArguments().parse()
 
-        if not os.path.exists('./' + deck_name):
-                os.makedirs('./' + deck_name)
+        file_name = cliArguments.getArgument(CLIArguments.CARDS_LIST_FILE_PATH)
 
-        with open(png_name, 'wb') as out_file:
-                shutil.copyfileobj(response.raw, out_file)
-        del response
+
+        with open(file_name) as file:
+            lines = list(file)
+            deck_name = file_name.split(".")[0]
+
+            for line in lines:
+                strings = line.split(" ")
+                del strings[0]
+                card_name = "".join(strings)
+
+                URL = "https://api.scryfall.com/cards/search"
+
+                PARAMS = {'q':card_name}
+
+                print("Pulling {} card.....".format(card_name))
+                r = requests.get(url = URL, params = PARAMS)
+
+                data = r.json()
+
+                png_img_uri=data['data'][0]['image_uris']['png']
+
+                response = requests.get(png_img_uri, stream=True)
+
+                if '//' in card_name:
+                    card_name = card_name.split('//')[0]
+                png_name = './' + deck_name + '/' + card_name + '.png'
+
+                if not os.path.exists('./' + deck_name):
+                        os.makedirs('./' + deck_name)
+
+                with open(png_name, 'wb') as out_file:
+                        shutil.copyfileobj(response.raw, out_file)
+                del response
+
+if __name__ == "__main__":
+    main = Main()
+    main.main()
+
+# file_name = sys.argv[1]
+#
+# with open(file_name) as file:
+#     lines = list(file)
+#     deck_name = file_name.split(".")[0]
+#
+#     for line in lines:
+#         strings = line.split(" ")
+#         del strings[0]
+#         card_name = "".join(strings)
+#
+#         URL = "https://api.scryfall.com/cards/search"
+#
+#         PARAMS = {'q':card_name}
+#
+#         print("Pulling {} card.....".format(card_name))
+#         r = requests.get(url = URL, params = PARAMS)
+#
+#         data = r.json()
+#
+#         png_img_uri=data['data'][0]['image_uris']['png']
+#
+#         response = requests.get(png_img_uri, stream=True)
+#
+#         if '//' in card_name:
+#             card_name = card_name.split('//')[0]
+#         png_name = './' + deck_name + '/' + card_name + '.png'
+#
+#         if not os.path.exists('./' + deck_name):
+#                 os.makedirs('./' + deck_name)
+#
+#         with open(png_name, 'wb') as out_file:
+#                 shutil.copyfileobj(response.raw, out_file)
+#         del response
